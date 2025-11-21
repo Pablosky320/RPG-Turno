22c51066f8c43788a38ba384713e150a43dd4dc1
Pablosky320 <pgb1909@gmail.com>
2025-11-21 14:32:13 +0100

    Ayuda

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -2,19 +2,67 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    public List Units;
+    [Header("Propiedade di la Unitad")]
+    [SerializeField] string characterName;
 
-    public bool hasActed;
+    public bool hasActed = true;
+    bool hasAtacked = false;
+    bool hasMoved = false;
+    public bool isFriendly;
+    ClickToMove clickTomove;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Awake()//Esto es pa que no me deje clickar fuera de turno o al darle al play.
     {
-        
+        clickTomove = GetComponent<ClickToMove>();
+        clickTomove.enabled = false;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Run()
     {
-        
+        if (hasMoved  hasAtacked) //Aquí si uno de los dos ha ocurrido la funcion acaba.
+        {
+            return;
+        }
+        if (isFriendly) // Si es una unidad aliada, dejala moverse y cambiale la posición en el momento correcto.
+        {
+            clickTomove.enabled = true;
+            clickTomove.destinoDumie.position = transform.position;
+            Debug.Log(characterName + "esta scouting.");
+        }
+        else
+        {
+            Debug.Log(characterName + "esta bailando salsa"); //Um firi fassendo barras
+        }
+    }
+    public void Atack()
+    {
+        if (hasActed  hasAtacked)
+        {
+            return;
+        }
+        Debug.Log(characterName + "esta liandose a piñas");
+        FinishAtack();
+    }
+    public void EndTurn()
+    {
+        if (hasActed)
+        {
+            return;
+        }
+        Debug.Log(characterName + "dice que ya no puede mas");
+        FinishAction();
+    }
+    public void FinishMovement()
+    {
+        clickTomove.enabled = false;
+        hasMoved = true;
+    }
+    public void FinishAtack()
+    {
+        hasActed = true;
+    }
+    public void FinishAction()
+    {
+        hasActed = true;
+        TurnManager.Instance.TurnEnded();
     }
 }
--- Assets/Scripts/UnitSelectionToggle.cs
+++ Assets/Scripts/UnitSelectionToggle.cs
@@ -29,4 +29,9 @@ public class UnitSelectionToggle : MonoBehaviour
         
 
     }
+
+    void deactivateAllUI()
+    {
+
+    }
 }
